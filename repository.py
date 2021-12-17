1cff9786be7aa010dbb413674ac00c5d6733eeda
Matheus Santiago <matheus.n.santiago99@gmail.com>
2021-12-16 22:40:35 -0300

    botar credenciais do google como env variable

--- repository.py
+++ repository.py
@@ -3,13 +3,25 @@ from model import Publicacao
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-import json
-import os
 import datetime
+import os
 
 class SumulaDB:
     def __init__(self) -> None:
-        cred = credentials.Certificate(json.loads(os.environ["CRED"]))
+        cred = credentials.Certificate(
+            {
+                "type": "service_account",
+                "project_id": "sumula-dou",
+                "private_key_id": "83ab6174f69ce38bc55133d7ef9507e86f5f5e52",
+                "private_key": os.environ["PRIVATE_KEY"],
+                "client_email": "firebase-adminsdk-hdk8k@sumula-dou.iam.gserviceaccount.com",
+                "client_id": os.environ["CLIENT_ID"],
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-hdk8k%40sumula-dou.iam.gserviceaccount.com",
+            }
+        )
         firebase_admin.initialize_app(cred, {"projectId": "sumula-dou"})
         self.db = firestore.client()
 
